d6880a9bb0245eb4be756a1f260e5c5c15c87370
曾磊 <leizeng@cengleis-MacBook-Pro.local>
2016-11-09 22:07:50 +0800

    refactor swagger middleware

--- packages/wechat-app-server/src/index.js
+++ packages/wechat-app-server/src/index.js
@@ -1,51 +1,35 @@
 import restify from 'restify'
-import swagger from 'swagger-restify'
+import sessions from 'client-sessions'
 
 import './middlewares/db'
 import passport, { login } from './middlewares/passport-local'
-import { hello } from './apis/user'
 
 const server = restify.createServer()
 const port = 1108
 
 server.pre(restify.pre.userAgentConnection())
 server.use(restify.bodyParser())
-server.use(restify.queryParser({ mapParams: false }))
+server.use(restify.queryParser())
+
+server.use(sessions({
+    // cookie name dictates the key name added to the request object
+    cookieName: 'session-wechat-app-server',
+    // should be a large unguessable string
+    secret: 'blablalala',
+    // how long the session will stay valid in ms
+    duration: 365 * 24 * 60 * 60 * 1000
+}))
 
 passport(server)
+if (process.env.NODE_ENV === 'development') {
+  require('./middlewares/swagger')(server)
+}
 
 restify.defaultResponseHeaders = function(data) {
     this.header('Access-Control-Allow-Origin', '*')
 }
 
 server.post(/^\/login/, login)
-server.get(/^\/hello/, hello)
-
-swagger.init(server, {
-    swagger: '2.0', // or swaggerVersion as backward compatible
-    info: {
-      version: '1.0',
-      title: 'Swagger 2.0 Restify example'
-    },
-    tags: [{
-      name: 'example',
-      description: 'Just an example API'
-    }],
-    host: 'localhost:' + port,
-    apis: ['./src/apis/user.yml'],
-    produces: [
-      'application/json',
-      'text/xml'
-    ],
-    consumes: [
-      'application/json',
-      'text/xml'
-    ],
-    // swagger-restify proprietary
-    swaggerURL: '/swagger',
-    swaggerJSON: '/api-docs.json',
-    swaggerUI: './dist'
-});
 
 server.listen(port)
 
